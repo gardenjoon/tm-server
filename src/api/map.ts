a0d1f6f105cf760c23427792ba38c46c3467c23b
gardenjoon <wees114@gmail.com>
2023-07-28 15:00:57 +0900

    update map api

--- src/api/map.ts
+++ src/api/map.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import request from "request";
 import * as dotenv from "dotenv";
-import { crawler } from '../middlewares/crawling';
+import { crawler } from "../middlewares/crawling";
 import { AppDataSource, Controller, Entity } from "../../TodaysMenuDB";
 dotenv.config();
 const api_key = process.env.kakao_api_key;
@@ -16,25 +16,28 @@ const getAddress = async (req: Request, res: Response) => {
         },
     */
     try {
-        const api_url = 'https://dapi.kakao.com/v2/local/search/address.json?query=' + encodeURI(req.params.query);
+        const api_url = "https://dapi.kakao.com/v2/local/search/address.json?query=" + encodeURI(req.params.query);
         const options = {
             url: api_url,
             headers: {
-                Authorization: `KakaoAK ${api_key}`
-            }
+                Authorization: `KakaoAK ${api_key}`,
+            },
         };
 
         request.get(options, function (error, response, body) {
             if (!error && response.statusCode == 200) {
                 console.log(body);
-                res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
+                res.writeHead(200, { "Content-Type": "text/json;charset=utf-8" });
                 return res.end(body);
-              } else {
+            } else {
                 console.log(error);
-                return res.status(response.statusCode).json({
-                    message: "서버 에러입니다."
-                }).end();
-              }
+                return res
+                    .status(response.statusCode)
+                    .json({
+                        message: "서버 에러입니다.",
+                    })
+                    .end();
+            }
         });
     } catch (e) {
         return res.status(500).json({
@@ -59,27 +62,32 @@ const getRegion = async (req: Request, res: Response) => {
         },
     */
     try {
-        const api_url = encodeURI(`https://dapi.kakao.com/v2/local/geo/coord2regioncode.json?x=${req.params.x}&y=${req.params.y}`);
+        const api_url = encodeURI(
+            `https://dapi.kakao.com/v2/local/geo/coord2regioncode.json?x=${req.params.x}&y=${req.params.y}`
+        );
         //  + '&size=' + encodeURI(req.params.size);
         console.log(api_url);
         const options = {
             url: api_url,
             headers: {
-                Authorization: `KakaoAK ${api_key}`
-            }
+                Authorization: `KakaoAK ${api_key}`,
+            },
         };
 
         request.get(options, function (error, response, body) {
             if (!error && response.statusCode == 200) {
                 console.log(body);
-                res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
+                res.writeHead(200, { "Content-Type": "text/json;charset=utf-8" });
                 return res.end(body);
-              } else {
+            } else {
                 console.log(error);
-                return res.status(response.statusCode).json({
-                    message: "서버 에러입니다."
-                }).end();
-              }
+                return res
+                    .status(response.statusCode)
+                    .json({
+                        message: "서버 에러입니다.",
+                    })
+                    .end();
+            }
         });
     } catch (e) {
         return res.status(500).json({
@@ -88,8 +96,6 @@ const getRegion = async (req: Request, res: Response) => {
         });
     }
 };
-
-
 
 const search = async (req: Request, res: Response) => {
     /*  #swagger.tags = ['Map']
@@ -133,18 +139,18 @@ const search = async (req: Request, res: Response) => {
 
     try {
         let url = `https://dapi.kakao.com/v2/local/search/keyword.json?&category_group_code=FD6&query=${req.query.query}`;
-        req.query.x ? url +=  `&x=${req.query.x}` : null;
-        req.query.y ? url +=  `&y=${req.query.y}` : null;
-        req.query.radius ? url +=  `&radius=${req.query.radius}` : null;
-        req.query.rect ? url += `&rect=${req.query.rect}` : null;
-        req.query.size ? url += `&size=${req.query.size}` : null;
+        req.query.x ? (url += `&x=${req.query.x}`) : null;
+        req.query.y ? (url += `&y=${req.query.y}`) : null;
+        req.query.radius ? (url += `&radius=${req.query.radius}`) : null;
+        req.query.rect ? (url += `&rect=${req.query.rect}`) : null;
+        req.query.size ? (url += `&size=${req.query.size}`) : null;
 
         const api_url = encodeURI(url);
         const options = {
             url: api_url,
             headers: {
-                Authorization: `KakaoAK ${api_key}`
-            }
+                Authorization: `KakaoAK ${api_key}`,
+            },
         };
 
         const response = await new Promise<string>((resolve, reject) => {
@@ -154,31 +160,31 @@ const search = async (req: Request, res: Response) => {
             });
         });
 
-
         console.log("Getting KAKAO API Successful");
         let result = JSON.parse(response);
-        await Promise.all(result['documents'].map(async (store) => {
-           if (store['place_name'].includes('점')) {
-               let temp_name = store['place_name'].split(' ')
-               temp_name.pop()
-               store['place_name'] = temp_name.join(' ')
-            }
-            if (!await new Controller.ResMenu(Entity.ResMenu, AppDataSource.manager).checkMenu(store.id)) {
-                const menuList = await crawler(store.id);
-    
-                if (menuList != null) {
-                    await new Controller.ResMenu(Entity.ResMenu, AppDataSource.manager).saveMenu(menuList);
+        await Promise.all(
+            result["documents"].map(async (store) => {
+                if (store["place_name"].includes("점")) {
+                    let temp_name = store["place_name"].split(" ");
+                    temp_name.pop();
+                    store["place_name"] = temp_name.join(" ");
                 }
-            } else {
-                store['menus'] = await new Controller.ResMenu(Entity.ResMenu, AppDataSource.manager).getMenu(store.id);
-            };
-        }))
+                if (!(await new Controller.ResMenu(Entity.ResMenu, AppDataSource.manager).checkMenu(store.id))) {
+                    const menuList = await crawler(store.id);
 
+                    if (menuList != null) {
+                        await new Controller.ResMenu(Entity.ResMenu, AppDataSource.manager).saveMenu(menuList);
+                    }
+                } else {
+                    store["menus"] = await new Controller.ResMenu(Entity.ResMenu, AppDataSource.manager).getMenu(
+                        store.id
+                    );
+                }
+            })
+        );
 
- 
-        res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
+        res.writeHead(200, { "Content-Type": "text/json;charset=utf-8" });
         return res.end(JSON.stringify(result));
-
 
         // request.get(options, function (error, response, body) {
         //     if (!error && response.statusCode == 200) {
@@ -192,7 +198,7 @@ const search = async (req: Request, res: Response) => {
         //             }
         //         })
         //         await crawler()
-                
+
         //         res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
         //         return res.end(JSON.stringify(result));
         //       } else {
@@ -209,6 +215,5 @@ const search = async (req: Request, res: Response) => {
         });
     }
 };
-
 
 export { getAddress, getRegion, search };
